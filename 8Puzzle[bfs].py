15d3fb61d7d8b1a5a6206f89352980b30166bbce
Deep Contractor <31130563+chesterking123@users.noreply.github.com>
2020-03-22 23:59:18 +0530

    Update 8Puzzle[bfs].py

--- 8Puzzle[bfs].py
+++ 8Puzzle[bfs].py
@@ -8,29 +8,23 @@ import numpy as np
 
 class Node:
     def __init__(self,puz):
-        #puzzle is the arrangement of the digits
-        #children is the frontier or the nodes to be explored
-        #parent is the current node
-        #pos is the position of the tile containing 0
+      
         self.puzzle = puz[:]
         self.children = []  
         self.parent = None
         self.pos = 0
         self.level = 0
     
-    #Function for taking user-input
     def TakeInput():
         print("Input numbers row-wise; i.e. 1 2 3 4 5 6 7 8 0: ")
         new_puzzle = [int(x) for x in input().split()]
         return new_puzzle
     
-    #Function to check the digit zero in the puzzle    
     def CheckZero(self,puz):
         for i in range(0,9):
             if puz[i] == 0:
                 return i
    
-    #Function to check if goal state is reached
     def CheckGoal(self):
         GoalFlag = True
         goal = [1,2,3,4,5,6,7,8,0]
@@ -39,7 +33,6 @@ class Node:
             return GoalFlag
         return GoalFlag
     
-    #Function to move slide up
     def SlideUp(self):
         if(self.pos!=0 and self.pos!=1 and self.pos!=2):
             puz = self.puzzle[:]
@@ -48,14 +41,11 @@ class Node:
             puz[self.pos] = puz[self.pos-3]
             puz[self.pos-3] = temp
             child = Node(puz)
-            #child = Node(puz, self.level + 1)
-            #append to frontier/children list
+          
             self.children.append(child)
-            #self.level = self.level + 1
-            #make parent node as current node
+         
             child.parent = self
     
-    #Function to move slide right
     def SlideRight(self):
         if(self.pos%3 < 2):
             puz = self.puzzle[:]
@@ -63,12 +53,9 @@ class Node:
             puz[self.pos] = puz[self.pos+1]
             puz[self.pos+1] = temp
             child = Node(puz)
-            #child = Node(puz, self.level + 1)
             self.children.append(child)
-            #self.level = self.level + 1
             child.parent = self
     
-    #Function to move slide down
     def SlideDown(self):
         if(self.pos!=6 and self.pos!=7 and self.pos!=8):
             puz = self.puzzle[:]
@@ -76,12 +63,9 @@ class Node:
             puz[self.pos] = puz[self.pos+3]
             puz[self.pos+3] = temp
             child = Node(puz)
-            #child = Node(puz, self.level + 1)
             self.children.append(child)
-            #self.level = self.level + 1
             child.parent = self
     
-    #Function to move slide left
     def SlideLeft(self):
         if(self.pos%3 != 0):
             puz = self.puzzle[:]
@@ -89,34 +73,26 @@ class Node:
             puz[self.pos] = puz[self.pos-1]
             puz[self.pos-1] = temp
             child = Node(puz)
-            #child = Node(puz, self.level + 1)
             self.children.append(child)
-            #self.level = self.level + 1
             child.parent = self
     
-    #Function to slide tile in all possible directions, to generaye child nodes
     def PuzzleIteration(self):
         self.pos = self.CheckZero(self.puzzle)        
         self.SlideRight()
         self.SlideDown()
         self.SlideLeft()
         self.SlideUp()
-        #print("iter")
     
-    #Function to display puzzle 
     def DisplayPuzzle(self):
         print()
         c = 0
         for i in range(3):
             for j in range(3):
                 print(self.puzzle[c], end = " ")
-                #print(" ")
                 c = c + 1
             print()    
     
-#Breadth first search algorithm to solve the 8 puzzle        
 def BFSalgo(root):
-    #set used for quick searching of visited states
     visited_nodes = set()
     frontier = []
     visited_nodes.add(str(root.puzzle))
@@ -125,11 +101,9 @@ def BFSalgo(root):
     #return 0
     while True:        
         CurrentPuzzle = frontier.pop(0)
-        #Check if goal position is reached
         if CurrentPuzzle.CheckGoal():
             solution = []
             solution.append(CurrentPuzzle)
-            #Tracing the moves to get to goal position
             while CurrentPuzzle.parent != None:
                 CurrentPuzzle = CurrentPuzzle.parent
                 solution.append(CurrentPuzzle)               
@@ -137,12 +111,9 @@ def BFSalgo(root):
             print("Total configurations explored: "+str(len(visited_nodes)))
             print("Moves for reaching the goal state: "+str(len(solution)-1))
             return solution, visited_nodes
-        #Iterating all the possible moves for a particular node
         CurrentPuzzle.PuzzleIteration()
         for i in range(len(CurrentPuzzle.children)):
             ToBeExplored = CurrentPuzzle.children[i]
-            #Checking if iteration already explored, if not so it is added to
-            #explored
             if(not str(ToBeExplored.puzzle) in visited_nodes):
                 frontier.append(ToBeExplored)
                 visited_nodes.add(str(ToBeExplored.puzzle))                           
@@ -151,9 +122,7 @@ def BFSalgo(root):
 if __name__ == "__main__":
     count = 0
     puzzle = Node.TakeInput()
-    #Create object root from class Node
     root = Node(puzzle)
-    #check solvabiity
     for i in range(0,8):
         for j in range(i,9):
             if(puzzle[i] > puzzle[j] and puzzle[j] != 0):
@@ -169,7 +138,6 @@ if __name__ == "__main__":
         print()
         print("Writing to files; may take some time")
         print()
-        #Writing puzzle data to files
         file = open("nodePath.txt", "w")
         for i in range(len(sol)):
             temp = np.array(sol[i].puzzle).reshape((3,3))
@@ -181,7 +149,6 @@ if __name__ == "__main__":
         print("nodePath.txt updated")
         print()
         print("Now updating Nodes.txt")
-        #Writing explored nodes/puzzles to file
         file = open("Nodes.txt", "w")
         for i in range(len(visited)):
             temp2 = np.array(list(visited)[i])
